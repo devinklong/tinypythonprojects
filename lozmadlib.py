7f497af08ab9c1c6e7e88d1ab3b8bb3348fb6aec
Devin L <longdevin2@gmail.com>
2022-06-15 13:31:56 -0500

    Add files via upload

---
+++ lozmadlib.py
@@ -0,0 +1,12 @@
+#madlibgame - working on concatenation
+
+adj = input("Put your adjective here: ")
+verb = input("Put your verb here: ")
+item = input("Put your item here: ")
+famous_person = input("Famous person: ")
+
+madlib = f"The Legend of Zelda is so {adj}! It is an incredible video game franchise series where \
+    I can {verb} the world. I really like that you also can use the {item} because it makes me \
+    feel like I am {famous_person}!"
+
+print(madlib)
