a1f80eb8ea9a6398893988bc780540bc1c338690
Devin L <longdevin2@gmail.com>
2022-08-05 15:53:20 -0500

    Add files via upload

---
+++ sddascmii.py
@@ -0,0 +1,27 @@
+#Sweetdiddlydee Ascmii 
+
+print(" SSS  W    W EEEEE  EEEEE TTTTT")
+print("S   S W    W E      E       T  ")
+print("S     W    W E      E       T  ")
+print("SSS   W WW W EEE    EEE     T  ")
+print("    S WW  WW E      E       T  ")
+print("S   S WW  WW E      E       T  ")
+print(" SSS  W    W EEEEE  EEEEE   T  \n")
+
+print("DDDD  IIIII DDDD  DDDD  L    Y    Y")
+print("D   D   I   D   D D   D L     Y  Y ")
+print("D   D   I   D   D D   D L      YY  ")
+print("D   D   I   D   D D   D L      YY  ")
+print("D   D   I   D   D D   D L      YY  ")
+print("D   D   I   D   D D   D L      YY  ")
+print("DDDD  IIIII DDDD  DDDD  LLLLL  YY  \n")
+\
+print("DDDD  EEEEE EEEEE")
+print("D   D E     E    ")
+print("D   D E     E    ")
+print("D   D EEE   EEE  ")
+print("D   D E     E    ")
+print("D   D E     E    ")
+print("DDDD  EEEEE EEEEE")
+
+
